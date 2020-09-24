aaef1db5cdc76ac1c3681393a16f31c795707865
Perestoronin Pavel <p_perestoronin@mail.ru>
2020-09-25 02:42:09 +0300

    FIX: query 25

--- lab_02/query25.sql
+++ lab_02/query25.sql
@@ -6,11 +6,16 @@ AS (
     SELECT player_id, MAX(goals)
     FROM game_skaters_stats
     GROUP BY player_id
+),
+rnum AS (
+    SELECT firstName, goals AS MaxGoals,
+    AVG(goals) OVER (PARTITION BY firstName) AS AvgGoals,
+    ROW_NUMBER() OVER(PARTITION BY firstName, goals) AS rn
+    FROM player_info
+    JOIN CoolPlayers
+    ON player_info.player_id = CoolPlayers.player_id
 )
 
-SELECT firstName, goals AS MaxGoals,
-AVG(goals) OVER (PARTITION BY firstName) AS AvgGoals,
-ROW_NUMBER() OVER(PARTITION BY firstName, goals, AVG(goals)) AS rn
-FROM player_info
-JOIN CoolPlayers
-ON player_info.player_id = CoolPlayers.player_id;
+SELECT firstName, MaxGoals, AvgGoals
+FROM rnum
+WHERE rn = 1;
