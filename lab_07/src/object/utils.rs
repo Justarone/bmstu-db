ba7a61f9f52f710ebed0852bc1560e7479f4d96b
Perestoronin Pavel <p_perestoronin@mail.ru>
2020-12-05 01:04:03 +0300

    ADD: seems like object linq is done

--- lab_07/src/object/utils.rs
+++ lab_07/src/object/utils.rs
@@ -6,10 +6,10 @@ const MAIN_QUERY: &str = "SELECT * FROM player_info";
 pub(super) const NQUERIES: usize = 5;
 pub(super) const RUNNABLE_OBJECT_QUERIES: [fn(&[PlayerInfo]); NQUERIES] = [
     show_young_russians,
-    show_young_russians,
-    show_young_russians,
-    show_young_russians,
     nationalities_and_cities,
+    hockey_nations,
+    oldest_player,
+    players_from_moscow,
 ];
 
 pub(super) fn get_all_players(
@@ -37,7 +37,7 @@ pub(super) fn get_all_players(
 // FROM
 // WHERE
 fn show_young_russians(players: &[PlayerInfo]) {
-    println!("SHOW YOUNG RUSSIAN PLAYERS");
+    println!("\nSHOW YOUNG RUSSIAN PLAYERS");
     for player in players.iter().filter_map(|p| {
         if p.nationality == "RUS" && p.birth_date > NaiveDate::from_ymd(1995, 1, 1) {
             Some(format!(
@@ -58,7 +58,7 @@ fn show_young_russians(players: &[PlayerInfo]) {
 // FROM
 // ORDER BY
 fn nationalities_and_cities(players: &[PlayerInfo]) {
-    println!("AGGREGATION BY NATIONALITY AND BEST CITY FOR EVERY NATION");
+    println!("\nAGGREGATION BY NATIONALITY AND BEST CITY FOR EVERY NATION");
     let mut nationality_cities = HashMap::new();
     for (nationality, group) in &players
         .iter()
@@ -75,16 +75,57 @@ fn nationalities_and_cities(players: &[PlayerInfo]) {
     }
 
     for (nationality, cities) in nationality_cities.iter() {
-        let (best_city, count) = cities.iter().fold((String::from(""), 0), |acc, (city, count)| {
-            if acc.1 < *count {
-                (city.to_string(), *count)
-            } else {
-                acc
-            }
-        });
+        let (best_city, count) = cities
+            .iter()
+            .fold((String::from(""), 0), |acc, (city, count)| {
+                if acc.1 < *count {
+                    (city.to_string(), *count)
+                } else {
+                    acc
+                }
+            });
         println!("{:<4} - {} ({})", nationality, best_city, count);
     }
 }
 
+// SELECT
+// FROM
+// GROUP BY
 // HAVING
 // ORDER BY
+fn hockey_nations(players: &[PlayerInfo]) {
+    println!("\nHOCKEY NATIONS (MORE THAN 10 PLAYERS IN NHL)");
+    for (nationality, count) in players
+        .iter()
+        .map(|p| p.nationality.clone())
+        .sorted()
+        .group_by(|n| n.clone())
+        .into_iter()
+        .filter_map(|(n, g)| {
+            let count = g.count();
+            if count > 10 {
+                Some((n, count))
+            } else {
+                None
+            }
+        })
+    {
+        println!("{:<5}({:^4})", nationality, count);
+    }
+}
+
+fn oldest_player(players: &[PlayerInfo]) {
+    println!("\nOLDEST PLAYER IN THIS DATABASE");
+    let oldest_player = players.iter().min_by_key(|p| p.birth_date).unwrap();
+    println!(
+        "Oldest player is {} {} ({:?})",
+        oldest_player.first_name, oldest_player.last_name, oldest_player.birth_date
+    );
+}
+
+fn players_from_moscow(players: &[PlayerInfo]) {
+    println!("PLAYERS FROM MOSCOW");
+    for p in players.iter().filter(|p| p.birth_city == "Moscow") {
+        println!("{:<20} {:<20} {:?}", p.first_name, p.last_name, p.birth_date);
+    }
+}
